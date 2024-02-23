d1343dc0ab4b92169a28010807a095a2caaee21a
vojtech.stanek <vojtech.stanek@thermofisher.com>
2024-02-23 07:04:38 +0100

    Add months

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -37,3 +37,18 @@ export const ONE_TIME_OPTIONS: SelectItem[] = [
   { id: 0, name: 'Periodically' },
   { id: 1, name: 'One time' },
 ];
+
+export const MONTHS: SelectItem[] = [
+  { id: 0, name: 'January' },
+  { id: 1, name: 'February' },
+  { id: 2, name: 'March' },
+  { id: 3, name: 'April' },
+  { id: 4, name: 'May' },
+  { id: 5, name: 'June' },
+  { id: 6, name: 'July' },
+  { id: 7, name: 'August' },
+  { id: 8, name: 'September' },
+  { id: 9, name: 'October' },
+  { id: 10, name: 'November' },
+  { id: 11, name: 'December' },
+];
